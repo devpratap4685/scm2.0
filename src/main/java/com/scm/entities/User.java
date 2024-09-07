77c56883475af49c87bab340cd233c882db92f46
ayu-sh-kr <akjaiswal2003@gmail.com>
2024-09-07 18:00:16 +0530

    Update User.java: Code refactoring

--- src/main/java/com/scm/entities/User.java
+++ src/main/java/com/scm/entities/User.java
@@ -1,4 +1,5 @@
 package com.scm.entities;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -11,10 +12,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import java.util.*;
 
-@Entity(name="user")
-@Table(name="users")
+@Entity(name = "user")
+@Table(name = "users")
 @Getter
 @Setter
 @AllArgsConstructor
@@ -23,30 +25,28 @@ import java.util.*;
 public class User {
     @Id
     private String userId;
-    @Column(name="user_name",nullable=false)
+    @Column(name = "user_name", nullable = false)
     private String name;
-    @Column(unique=true,nullable=false)
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
-    @Column(length=10000)
+    @Column(length = 10000)
     private String about;
-    @Column(length=1000)
+    @Column(length = 1000)
     private String profilePic;
     private String phonenumber;
-    
+
     // information
-    private boolean enabled=false;
-    private boolean emailVerified=false;
-    private boolean phoneVerified=false;
+    private boolean enabled = false;
+    private boolean emailVerified = false;
+    private boolean phoneVerified = false;
 
     // self,google,facebook,twitter,linkedin,github
-    private Providers provider=Providers.SELF;
+    private Providers provider = Providers.SELF;
     private String providerUserId;
 
-    @OneToMany(mappedBy="user",cascade=CascadeType.ALL,fetch=FetchType.LAZY,orphanRemoval=true)
-    private List<Contact>contacts=new ArrayList<>();
-
-   
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Contact> contacts = new ArrayList<>();
 
 
 }
